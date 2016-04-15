ea5251eade150b83315bf3d0f630ca100cb3d239
leonardo <leocr03@gmail.com>
2016-04-14 22:06:52 -0300

    Creating fail test to open issue.

--- src/test/java/com/camel/jetty/test/CamelJettyTest.java
+++ src/test/java/com/camel/jetty/test/CamelJettyTest.java
@@ -1,9 +1,8 @@
-package com.zenvia;
+package com.camel.jetty.test;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.jetty.JettyHttpComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,15 +18,13 @@ public class CamelJettyTest {
 
             @Override
             public void configure() throws Exception {
-                JettyHttpComponent jettyComponent = this.getContext().getComponent("jetty", JettyHttpComponent.class);
-                jettyComponent.setMinThreads(1);
-                jettyComponent.setMaxThreads(10);
-
                 from("timer://foo?period=1000")
                         .setHeader(Exchange.HTTP_URI, constant("https://www.google.com.br/?gfe_rd=cr&ei=qq4OV6rLH-Sp8wezhqpI&gws_rd=ssl"))
-                        .toF("jetty:http://local?httpClient.connectTimeout=%d&httpClient.timeout=%d&httpClient.maxRedirects=1",
+                        .toF("jetty:http://local?httpClient.connectTimeout=%d&httpClient.timeout=%d&httpClient.maxRedirects=1&httpClientMinThreads=%d&httpClientMaxThreads=%d",
                                 30000,
-                                30000)
+                                30000,
+                                8,
+                                16)
                         .log("${headers.CamelHttpUri}")
                         .log("*** the body is = [${body}]")
                         .end();
